759f154c505c08f5dbc8a276082411fa467a6335
oleksandrmazurok <mazurok0120@gmail.com>
2020-09-21 15:23:41 +0300

    end

---
+++ main.js
@@ -0,0 +1,5 @@
+
+document.querySelector('#menu-toggle').onclick = function(){
+    document.querySelector('#mobile-nav').classList.toggle('mobile-nav--hide');
+    document.querySelector('#mobile-nav').classList.toggle('mobile-nav--show');
+}
